7f18d548fba6852ce7afb0c235170601ccacdb1c
truong <truonghb29@gmail.com>
2023-03-29 15:16:53 +0700

    baitap

---
+++ Buoi1/Bai3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+long tinhXn(int);
+long tinhYn(int);
+
+long tinhXn(int n)
+{
+    if(n == 0)
+        return 1;
+    return tinhXn(n - 1) + tinhYn(n - 1);
+}
+
+long tinhYn(int n)
+{
+    if(n == 0)
+        return 0;
+    return 3*tinhXn(n - 1) + 2*tinhYn(n - 1);
+}
+
+int main()
+{
+    int n;
+    cout << "Nhap n: ";
+    cin >> n;
+
+    long kq1 = tinhXn(n);
+    long kq2 = tinhYn(n);
+
+    cout << "X(n) = " << kq1 << ", Y(n) = " << kq2 << endl;
+
+    return 0;
+}
